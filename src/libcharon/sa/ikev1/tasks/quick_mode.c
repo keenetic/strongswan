f694773fa795b0bd74e10fbdd9466c94edb42abd
Ilya Ponetayev <i.ponetaev@ndmsystems.com>
2015-08-07 21:59:39 +0300

    case 5949: split error notifications between IPsec phases

--- src/libcharon/sa/ikev1/tasks/quick_mode.c
+++ src/libcharon/sa/ikev1/tasks/quick_mode.c
@@ -966,7 +966,13 @@ static bool has_notify_errors(private_quick_mode_t *this, message_t *message)
 				DBG1(DBG_IKE, "received %N notify", notify_type_names, type);
 			}
 			if (type == NO_PROPOSAL_CHOSEN) {
-				charon->bus->alert(charon->bus, ALERT_PROPOSAL_MISMATCH_IKEV1);
+				if (this->ike_sa->get_state(this->ike_sa) == IKE_ESTABLISHED)
+				{
+					charon->bus->alert(charon->bus, ALERT_PROPOSAL_MISMATCH_IKEV1_IPSEC);
+				} else
+				{
+					charon->bus->alert(charon->bus, ALERT_PROPOSAL_MISMATCH_IKEV1_IKE);
+				}
 			}
 		}
 	}
