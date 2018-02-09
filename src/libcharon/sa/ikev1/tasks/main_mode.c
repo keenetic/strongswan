17b7123bbaeabea13055e4132bf7d087e9d11925
Ilya Ponetayev <i.ponetaev@ndmsystems.com>
2018-02-09 16:00:47 +0300

    add alerts when no proposal is chosen in IKEv1

--- src/libcharon/sa/ikev1/tasks/main_mode.c
+++ src/libcharon/sa/ikev1/tasks/main_mode.c
@@ -419,6 +419,7 @@ METHOD(task_t, process_r, status_t,
 			if (!this->proposal)
 			{
 				DBG1(DBG_IKE, "no proposal found");
+				charon->bus->alert(charon->bus, ALERT_PROPOSAL_MISMATCH_IKEV1_IKE);
 				return send_notify(this, NO_PROPOSAL_CHOSEN);
 			}
 			this->ike_sa->set_proposal(this->ike_sa, this->proposal);
@@ -441,11 +442,13 @@ METHOD(task_t, process_r, status_t,
 										DIFFIE_HELLMAN_GROUP, &group, NULL))
 			{
 				DBG1(DBG_IKE, "DH group selection failed");
+				charon->bus->alert(charon->bus, ALERT_PROPOSAL_MISMATCH_IKEV1_IKE);
 				return send_notify(this, INVALID_KEY_INFORMATION);
 			}
 			if (!this->ph1->create_dh(this->ph1, group))
 			{
 				DBG1(DBG_IKE, "negotiated DH group not supported");
+				charon->bus->alert(charon->bus, ALERT_PROPOSAL_MISMATCH_IKEV1_IKE);
 				return send_notify(this, INVALID_KEY_INFORMATION);
 			}
 			if (!this->ph1->get_nonce_ke(this->ph1, message))
@@ -464,6 +467,7 @@ METHOD(task_t, process_r, status_t,
 			if (!id_payload)
 			{
 				DBG1(DBG_IKE, "IDii payload missing");
+				charon->bus->alert(charon->bus, ALERT_GENERAL_ERROR);
 				return send_notify(this, INVALID_PAYLOAD_TYPE);
 			}
 			id = id_payload->get_identification(id_payload);
